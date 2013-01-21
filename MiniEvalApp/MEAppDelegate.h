52d094c4cd7a27581d7c5959072d666ed13a31f3
Trieu Khang <trieukhang.hoang@2359media.us>
2013-01-21 10:05:33 +0700

    Initial Commit

---
+++ MiniEvalApp/MEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MEAppDelegate.h
+//  MiniEvalApp
+//
+//  Created by viet on 1/21/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiniEvalApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MiniEvalApp
+//
+//  Created by viet on 1/21/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MEAppDelegate class]));
+    }
+}
